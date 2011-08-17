2a3c9d5d23bc6208adf854798f206e652797383b
fsoderblom <fsoderblom@1407c404-adbb-11de-b66c-2b8a8bb3ca59>
2011-08-17 08:51:01 +0000

    Bugfix for SLA summary bug.

--- html/inc/init.inc.php
+++ html/inc/init.inc.php
@@ -32,7 +32,7 @@ if(!defined("_init"))
 	include('inc/type.class.php');
 	include('inc/parents.class.php');
 
-	define("KAGETSU", "Kagetsu v1.0.3");
+	define("KAGETSU", "Kagetsu v1.0.4");
 
 	// Settings
 	define("DB_HOST", "localhost");					// Databasens adress / IP nr (Prod)
